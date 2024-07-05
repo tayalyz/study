6dfa7dca3df4b327d597ef03bba3808c20741948
tayal <tayalis.6107038@gmail.com>
2024-07-05 14:00:48 +0300

    task_28, изменено вычисление monthRate

--- task_28/src/main/java/org/example/Main.java
+++ task_28/src/main/java/org/example/Main.java
@@ -6,12 +6,15 @@ public class Main {
         double yearRate = 4f; // годовая процентная ставка
         int numOfMonth = 36; // количество месяцев, на который открыт вклад
         double monthRefill = 1000; // ежемесячное пополнение
-        double monthRate = yearRate/12/100; // вычислите месячную процентную ставку
+        double monthRate; // вычислите месячную процентную ставку
 
-        for (int i = 1; i <= numOfMonth; i++) { // дополните условие цикла
-            double depositInterest = roubles * monthRate; // вычислите доход от процентов
-            roubles += depositInterest; // добавьте доход от процентов
-            roubles += monthRefill; // учтите ежемесячное пополнение
+        for (int i = 1; i <= 3; i++) { // дополните условие цикла
+            for (int j = 12; j > 0; j--) {
+                monthRate = yearRate/j/100;
+                double depositInterest = roubles * monthRate; // вычислите доход от процентов
+                roubles += depositInterest; // добавьте доход от процентов
+                roubles += monthRefill; // учтите ежемесячное пополнение
+            }
         }
 
         System.out.println("Через " + numOfMonth + " месяцев накопится " + Math.floor(roubles * 100) / 100 + " рублей");
